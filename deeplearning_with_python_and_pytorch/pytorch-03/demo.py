090cc58571ab2632d0dc80b5aed985918704fe54
zongzhicheng <842117866@qq.com>
2022-01-18 14:07:34 +0800

    zongzc_commit_2022/1/18

--- deeplearning_with_python_and_pytorch/pytorch-03/demo.py
+++ deeplearning_with_python_and_pytorch/pytorch-03/demo.py
@@ -16,6 +16,7 @@ import torch.optim as optim
 from torch import nn
 from tensorboardX import SummaryWriter
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 # 定义一些超参数
 train_batch_size = 64
@@ -42,6 +43,27 @@ class Net(nn.Module):
         x = F.relu(self.layer2(x))
         x = self.layer3(x)
         return x
+
+
+class Net1(torch.nn.Module):
+    def __init__(self):
+        super(Net1, self).__init__()
+        self.conv = torch.nn.Sequential(
+            OrderedDict(
+                [
+                    ("conv1", torch.nn.Conv2d(3, 32, 3, 1, 1)),
+                    ("relu1", torch.nn.ReLU()),
+                    ("pool", torch.nn.MaxPool2d(2))
+                ]
+            ))
+
+        self.dense = torch.nn.Sequential(
+            OrderedDict([
+                ("dense1", torch.nn.Linear(32 * 3 * 3, 128)),
+                ("relu2", torch.nn.ReLU()),
+                ("dense2", torch.nn.Linear(128, 10))
+            ])
+        )
 
 
 if __name__ == '__main__':
@@ -76,6 +98,7 @@ if __name__ == '__main__':
     model.to(device)
 
     # 定义损失函数和优化器
+    # 交叉熵
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
@@ -86,7 +109,7 @@ if __name__ == '__main__':
     eval_acces = []
     writer = SummaryWriter(log_dir='logs', comment='train-loss')
 
-    for epoch in range(num_epoches):
+    for epoch in range(2):
         train_loss = 0
         train_acc = 0
         model.train()
@@ -97,17 +120,28 @@ if __name__ == '__main__':
         for img, label in train_loader:
             img = img.to(device)
             label = label.to(device)
+            # 原来的 img.shape 为 torch.Size([64, 1, 28, 28])
+            # 变换后 img.shape 为torch.Size([64, 784])
+            # view(img.size(0),-1)会把除了第0维的其他维度变成1维
+            # view(-1)会把全部维度变成1维
+            # view(img.size(0),img.size(1),-1)会把除了第0,1维的其他维度变为1维
             img = img.view(img.size(0), -1)
             # 前向传播
             out = model(img)
             loss = criterion(out, label)
-            # 反向传播
+            # 清空梯度
+            # 缺省情况梯度是累加的，在梯度反向传播前，先需把梯度清零
             optimizer.zero_grad()
+            # 反向传播
             loss.backward()
+            # 更新参数
+            # 基于当前梯度（存储在参数的._grad属性中）更新参数
             optimizer.step()
             # 记录误差
+            # loss.item(): Tensor -> number
             train_loss += loss.item()
             # 保存loss的数据与epoch数值
+            # 在一个图表中记录一个标量的变化
             writer.add_scalar('Train', train_loss / len(train_loader), epoch)
             # 计算分类的准确率
             _, pred = out.max(1)
@@ -144,7 +178,7 @@ if __name__ == '__main__':
 
     plt.title('train loss')
     plt.plot(np.arange(len(losses)), losses)
-    # plt.plot(np.arange(len(eval_losses)), eval_losses)
-    # plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
-    plt.legend(['Train Loss'], loc='upper right')
+    plt.plot(np.arange(len(eval_losses)), eval_losses)
+    plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+    # plt.legend(['Train Loss'], loc='upper right')
     plt.show()
