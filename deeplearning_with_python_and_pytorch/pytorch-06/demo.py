be1fcb630f77ac860f1abfdd5dbee391ad177ac4
zongzhicheng <842117866@qq.com>
2022-02-10 19:00:20 +0800

    zongzc_commit_2022/2/10

--- deeplearning_with_python_and_pytorch/pytorch-06/demo.py
+++ deeplearning_with_python_and_pytorch/pytorch-06/demo.py
@@ -5,21 +5,14 @@
 """
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-import numpy as np
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from collections import Counter
 import os
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 # 定义一些超参数
-BATCHSIZE = 50
-DOWNLOAD_MNIST = False
 EPOCHES = 20
 LR = 0.001
 cfg = {
@@ -84,46 +77,36 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 # Model
 print('==> Building model..')
-
-net4 = VGG('VGG16')
-
-mlps = [net4.to(device)]
-
-optimizer = torch.optim.Adam([{"params": mlp.parameters()} for mlp in mlps], lr=LR)
-
+net = VGG('VGG16').to(device)
+# 交叉熵损失函数
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=LR)
 loss_function = nn.CrossEntropyLoss()
 
-for ep in range(EPOCHES):
+print('==> Begin train..')
+for epoch in range(EPOCHES):
+
+    running_loss = 0.0
     for img, label in trainloader:
         img, label = img.to(device), label.to(device)
-        optimizer.zero_grad()  # 10个网络清除梯度
-        for mlp in mlps:
-            mlp.train()
-            out = mlp(img)
-            loss = loss_function(out, label)
-            loss.backward()  # 网络们获得梯度
-        # 对所有的参数进行更新
+        # 权重参数梯度清零
+        optimizer.zero_grad()
+        net.train()
+        # 正向及反向传播
+        out = net(img)
+        loss = criterion(out, label)
+        loss.backward()
         optimizer.step()
 
-    pre = []
-    vote_correct = 0
-    mlps_correct = [0 for i in range(len(mlps))]
+    correct = 0
     for img, label in testloader:
         img, label = img.to(device), label.to(device)
-        for i, mlp in enumerate(mlps):
-            mlp.eval()
-            out = mlp(img)
+        net.eval()
+        out = net(img)
+        _, prediction = torch.max(out, 1)  # 按行取最大值
+        pre_num = prediction.cpu().numpy()
+        correct += (pre_num == label.cpu().numpy()).sum()
 
-            _, prediction = torch.max(out, 1)  # 按行取最大值
-            pre_num = prediction.cpu().numpy()
-            mlps_correct[i] += (pre_num == label.cpu().numpy()).sum()
+    print("VGG16模型迭代" + str(epoch) + "次的正确率为：" + str(correct / len(testloader)))
 
-            pre.append(pre_num)
-        arr = np.array(pre)
-        pre.clear()
-        result = [Counter(arr[:, i]).most_common(1)[0][0] for i in range(BATCHSIZE)]
-        vote_correct += (result == label.cpu().numpy()).sum()
-    # print("epoch:" + str(ep) + "集成模型的正确率" + str(vote_correct / len(testloader)))
-
-    for idx, coreect in enumerate(mlps_correct):
-        print("VGG16模型迭代" + str(ep) + "次的正确率为：" + str(coreect / len(testloader)))
+print('==> Finish train')
